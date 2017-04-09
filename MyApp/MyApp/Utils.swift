80cb459dc1308238a2b363d243bd7ad14fb17252
16230091@qq.com <16230091@qq.com>
2017-04-09 21:24:36 +0800

    first

---
+++ MyApp/MyApp/Utils.swift
@@ -0,0 +1,15 @@
+//
+//  Utils.swift
+//  MyApp
+//
+//  Created by jianqiang on 17/4/9.
+//  Copyright © 2017年 jianqiang. All rights reserved.
+//
+
+import Foundation
+
+class Utils {
+    static func writeLog(msg: String) -> () {
+        print(msg)
+    }
+}
